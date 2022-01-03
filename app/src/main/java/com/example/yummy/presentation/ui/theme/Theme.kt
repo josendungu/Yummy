2ea1a58c55ba8cc60b500d03347d7c8278a7df3c
Joe <josendungu.jm@gmail.com>
2022-01-03 11:03:28 +0300

    Theme Configuration

--- app/src/main/java/com/example/yummy/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/yummy/presentation/ui/theme/Theme.kt
@@ -7,24 +7,19 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Red500,
+    primaryVariant = Red200,
+    secondary = White500,
+    background = DarkGrey
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Red500,
+    primaryVariant = Red200,
+    secondary = White500,
+    background = White700
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+
 )
 
 @Composable
